a85724e524b445930d4883f614832e3984e43d77
melvin figueroa <melproducc@gmail.com>
2018-12-13 09:07:53 -0400

    register

--- app/src/main/java/com/example/joel/apprestaurant/RegisterRestaurant.java
+++ app/src/main/java/com/example/joel/apprestaurant/RegisterRestaurant.java
@@ -3,13 +3,17 @@
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.joel.apprestaurant.utils.Data;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -18,6 +22,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,6 +37,8 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
     private GoogleMap mMap;
     private Geocoder geocoder;
     private TextView street;
+    private Button next;
+    private LatLng mainposition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +51,37 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
         map.getMapAsync(this);
         geocoder =  new Geocoder(getBaseContext(), Locale.getDefault());
         street = findViewById(R.id.street);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                sendData();
+            }
+        });
+
+    }
+    public void sendData(){
+        TextView  name = findViewById(R.id.name);
+        TextView  nit = findViewById(R.id.nit);
+        TextView  street = findViewById(R.id.street);
+        TextView  property = findViewById(R.id.property);
+        TextView  phone = findViewById(R.id.telf);
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("authoritation", Data.TOKEN);
+        RequestParams params = new RequestParams();
+        params.add("name", name.getText().toString());
+        params.add("nit", nit.getText().toString());
+        params.add("street", street.getText().toString());
+        params.add("property", property.getText().toString());
+        params.add("phone", phone.getText().toString());
+        params.add("Lat", String.valueOf(mainposition.latitude));
+        params.add("Log", String.valueOf(mainposition.longitude));
+
+        client.post(Data.REGISTER_RESTAURANT, params, new JsonHttpResponseHandler(){
+            public void onSuccess(int statusCOde, PreferenceActivity.Header[] headers, JSONObject response){
+                AlertDialog.Builder diaglog =new AlertDialog.Builder(RegisterRestaurant.this);
+               // AsyncHttpClient.log.w(LOG_TAG, onSuccess(int,Header[],"JSONObject) was not overriden, but callback was received")
             }
         });
     }
@@ -60,10 +92,10 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
 
         // Add a marker in Sydney and move the camera
           //-19.5810667,-65.7633013
-        LatLng sydney = new LatLng(-19.5810667,-65.7633013);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Lugar").zIndex(17).draggable(true));
+        LatLng potosi = new LatLng(-19.5810667,-65.7633013);
+        mMap.addMarker(new MarkerOptions().position(potosi).title("Lugar").zIndex(17).draggable(true));
         mMap.setMinZoomPreference(16);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(potosi));
 
         mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
             @Override
@@ -78,6 +110,7 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
 
             @Override
             public void onMarkerDragEnd(Marker marker) {
+                mainposition = marker.getPosition();
                 String street_string = getStreet(marker.getPosition().latitude, marker.getPosition().longitude);
                 street.setText(street_string);
             }
@@ -88,10 +121,9 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
         String result ="";
 
         try {
-            address = geocoder.getFromLocation(lat,lon,2);
-            for (int i = 0; i < address.size(); i ++){
-            result += address.get(i).getThoroughfare() + " . ";
-            }
+            address = geocoder.getFromLocation(lat,lon,1);
+            result += address.get(0).getThoroughfare();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
