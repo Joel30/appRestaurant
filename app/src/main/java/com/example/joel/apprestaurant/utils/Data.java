c4815c31168a5161a11b1435c955c42b223ce317
melvin figueroa <melproducc@gmail.com>
2018-12-13 09:12:04 -0400

    registerrr

--- app/src/main/java/com/example/joel/apprestaurant/utils/Data.java
+++ app/src/main/java/com/example/joel/apprestaurant/utils/Data.java
@@ -1,7 +1,7 @@
 package com.example.joel.apprestaurant.utils;
 
 public class Data {
-    public static final String REGISTER_RESTAURANT = ;
+    public static final String REGISTER_RESTAURANT = "";
     public static String TOKEN = "Data eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoianVhbkBnZW1haWwuY29tIiwicGFzc3dvcmQiOiIxMjM0NTY3ODkiLCJpYXQiOjE1NDQ2Nzk0MjR9.h1x_oYGg-c_ZZ2UazkQLBwWIhySUKrynRIPZyccUVtk";
     public static String HOST = "http://192.168.1.3:7777/api/v1.0";
     public static String REGISTER_CLIENT = HOST + "/client";
